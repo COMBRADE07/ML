574633204fe97a6e69aa659e1fcf575c3172039a
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-09-17 12:09:40 +0530

    Update main.py

--- Ml_revision/Hyper Tunning and GridSearchCV/main.py
+++ Ml_revision/Hyper Tunning and GridSearchCV/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import GridSearchCV,train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 
 # load data
@@ -9,14 +12,13 @@ data = iris.feature_names
 df = pd.DataFrame(iris.data, columns=data)
 df['target'] = iris.target
 
-x = df.iloc[:,0:4]
-y = df.iloc[:,-1]
+x = df.iloc[:, 0:4]
+y = df.iloc[:, -1]
 # split dataset
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=10)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=10)
 
-
-# setting up gridsearchcv
-gridsearchcv = GridSearchCV(SVC(gamma='auto'),
+# setting up gridsearchcv for single algorithm
+'''gridsearchcv = GridSearchCV(SVC(gamma='auto'),
                             {
                                 'C':[1,10,15,20],
                                 'kernel':['rbf','linear','poly']
@@ -31,4 +33,47 @@ print(df)
 # finding best score and parameter
 score = gridsearchcv.best_score_
 para = gridsearchcv.best_params_
-print(f'best score is: {score}\nparameter: {para}')
+print(f'best score is: {score}\nparameter: {para}')'''
+
+# checking for multiple algorithms
+model_params = {
+    'svm': {
+        'model': SVC(gamma='auto'),
+        'params': {
+            'C': [1, 10, 20],
+            'kernel': ['rbf', 'linear', 'poly']
+        }
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params': {
+            'n_estimators': [1, 5, 10]
+        }
+    },
+    'logestic_regression': {
+        'model': LogisticRegression(),
+        'params': {
+            'C': [1, 10, 20],
+        }
+    },
+    'decision_tree': {
+        'model': DecisionTreeClassifier(),
+        'params': {
+            'criterion': ['gini', 'entropy', 'log_loss'],
+            'max_depth': [None, 1, 2, 3]
+        }
+    }
+}
+
+score = []
+for mn,mp in model_params.items():
+    gsv = GridSearchCV(mp['model'],mp['params'],cv=5,return_train_score=False)
+    gsv.fit(x,y)
+    score.append({
+        'model':mn,
+        'best_score':gsv.best_score_,
+        'best_params':gsv.best_params_
+    })
+
+sc = pd.DataFrame(data=score,columns=['model','best_score','best_params'])
+print(sc)
