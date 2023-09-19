c2a4472b0400953dc439d86c2d4d35c7086c88ff
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-09-19 12:45:01 +0530

    testing

--- Ml_revision/overfitting/l1.py
+++ Ml_revision/overfitting/l1.py
@@ -1,38 +1,24 @@
 # Lasso regularization | l1 regularization
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+iris = load_iris()
 
-# reading data
-data = pd.read_csv('housing_data.csv')
+df = pd.DataFrame(iris.data,columns=iris.feature_names)
+df['target'] = iris.target
+print(df.head())
 
-# data overview
-print(data.head())
-print(data.columns)
-print(data.describe())
+x = df.iloc[:,0:4].values
+y = df.iloc[:,-1].values
 
-
-# extracting dependent and independent variables
-x = data[['median_income']].values
-y = data[['median_house_value']].values
-
-
-# splitting dataset
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=2)
-
-print("length of x_train: ",len(x_train))
-print("length of x_test: ",len(x_test))
-
-# model building and training
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
 model = LinearRegression()
 model.fit(x_train,y_train)
 
-# test the model
-pred = model.predict(y_test)
-
-print(pred)
-
-# checking accuracy
-s = model.score(x_test,y_test)
-print(s*100)
+lasso_model = Lasso(alpha=2.0,max_iter=120,tol=0.1)
+lasso_model.fit(x_train,y_train)
+print(lasso_model.score(x_train,y_train))
+print(lasso_model.score(x_test,y_test))
--- Ml_revision/overfitting/l2.py
+++ Ml_revision/overfitting/l2.py
@@ -1,1 +1,24 @@
+# Rigde regularization | l2 regularization
 
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+iris = load_iris()
+
+df = pd.DataFrame(iris.data,columns=iris.feature_names)
+df['target'] = iris.target
+print(df.head())
+
+x = df.iloc[:,0:4].values
+y = df.iloc[:,-1].values
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+model = LinearRegression()
+model.fit(x_train,y_train)
+
+lasso_model = Ridge()
+lasso_model.fit(x_train,y_train) 
+print(lasso_model.score(x_train,y_train))
+print(lasso_model.score(x_test,y_test))
