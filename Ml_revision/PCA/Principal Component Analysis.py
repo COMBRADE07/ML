892ed6874a3f70f03e9d8a5ad8c43dc46a48d023
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-09-20 21:41:01 +0530

    Update Principal Component Analysis.py

--- Ml_revision/PCA/Principal Component Analysis.py
+++ Ml_revision/PCA/Principal Component Analysis.py
@@ -2,6 +2,8 @@
     Principal Component Analysis:
 
         - It is technique to reduce dimension
+        - It is a process of figuring out most important feature or Principal component
+          that has most impact on target variables.
 
 
 '''
