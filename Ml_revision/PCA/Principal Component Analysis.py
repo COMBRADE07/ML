474516b19ec2939c535dd7203cb2c1a3f09db13f
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-09-21 21:58:43 +0530

    final pca

--- Ml_revision/PCA/Principal Component Analysis.py
+++ Ml_revision/PCA/Principal Component Analysis.py
@@ -11,12 +11,14 @@ from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
 dataset = load_digits()
 
-df = pd.DataFrame(dataset.data,columns=dataset.feature_names)
+df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
 
 # df['target'] = dataset.target
-x = df.iloc[:,:].values
+x = df.iloc[:, :].values
 y = dataset.target
 
 # scaling is very important while performing PCA
@@ -24,9 +26,20 @@ scale = MinMaxScaler()
 x = scale.fit_transform(x)
 
 # splitting training and testing
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 model = LogisticRegression(max_iter=200)
-model.fit(x_train,y_train)
-print(model.score(x_train,y_train))
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test))
 
+# pca
+pca = PCA(.95)  # this will retain 95 % useful features
+x_pca = pca.fit_transform(x)
+print(x_pca.shape)
+print('total colums as feature: ',pca.n_components_)
+
+x_train, x_test, y_train, y_test = train_test_split(x_pca, y, test_size=0.2, random_state=0)
+
+modelx = LogisticRegression(max_iter=200)
+modelx.fit(x_train,y_train)
+print(modelx.score(x_test,y_test))
