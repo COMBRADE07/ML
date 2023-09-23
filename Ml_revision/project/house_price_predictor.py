d79e67e984871a6835d63f53d171b36ee4f7400c
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-09-23 23:34:58 +0530

    Update house_price_predictor.py

--- Ml_revision/project/house_price_predictor.py
+++ Ml_revision/project/house_price_predictor.py
@@ -4,3 +4,4 @@ import seaborn as sns
 from sklearn.preprocessing import LabelEncoder,StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression,LogisticRegression,Lasso,Ridge
+
