426bfb850e262c151f5864f578c483d393da4e25
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-10-01 19:42:23 +0530

    bias and variance text

---
+++ Ml_revision/Bias and Varience/main.py
@@ -0,0 +1,25 @@
+'''
+    # Bias:
+    - bias is a mesurement of how accurately a model
+      can capture a pattern in training dataset.
+    - train error
+    -
+    # Variance:
+    - test error
+
+
+    # overfit:
+    - variance is a mesurement of how accurately a model
+      can capture a pattern in training dataset
+    - model doesn't perform well on testing data
+    - high variance
+    - low bias
+
+
+    # underfit:
+    - model doesn't perform well on training data
+    - low variance
+    - high bias
+
+
+'''
