9ad7361a586c696404f2c31bc0c597391f364fda
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-10-02 21:57:23 +0530

    f

--- Ml_revision/Notes/supervised learning.py
+++ Ml_revision/Notes/supervised learning.py
@@ -1,7 +1,9 @@
 '''
 
     # Supervised ml:
-      - In supervised machine learning model are trained using label data. label data contains output tagged with it.
+      - In supervised machine learning model are trained using label data,
+        label data contains output tagged with it.
+
       
 
 '''
