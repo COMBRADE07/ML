5098832723a7944e7bae152c167b320158a2b46c
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-10-03 14:28:44 +0530

    Update supervised learning.py

--- Ml_revision/Notes/supervised learning.py
+++ Ml_revision/Notes/supervised learning.py
@@ -6,7 +6,29 @@
 
       - Types:
             a] Regression : used for predict the continuous value for example predict score,marks etc.
-            b] Classification : used for predict categorical values for example loan approval has only two categories  yes or no like that
-            
+            b] Classification : used for predict categorical values for example
+                loan approval has only two categories  yes or no like that.
+
+      - Model Evaluation metrics:
+        In summary, model evaluation metrics are essential tools in the machine learning lifecycle.
+         They help you assess, compare, and improve models, make informed decisions,
+         and ensure that models meet their intended objectives while
+         aligning with business or research goals.
+
+
+        i] regression metrics:
+        - Mean Absolute Error (MAE): MAE measures the average absolute difference between predicted and actual values.
+            It is less sensitive to outliers compared to MSE.
+
+        - Mean Squared Error (MSE): MSE measures the average squared difference between predicted and actual values.
+            It gives more weight to large errors and is sensitive to outliers.
+
+        - Root Mean Squared Error (RMSE): RMSE is the square root of MSE and provides an interpretable metric in
+            the same units as the target variable.
+
+        - R-squared (R2): R-squared measures the proportion of the variance in the dependent variable that is
+            predictable from the independent variables. It ranges from 0 to 1, where a higher value indicates a better fit.
+
+
 
 '''
