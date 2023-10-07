0159d9ba56175ad599e09f765146b9eaa5360699
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-10-07 20:56:07 +0530

    tranformer

--- Ml_revision/project/test .py
+++ Ml_revision/project/test .py
@@ -1,12 +1,24 @@
 import pandas as pd
-
+from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
-onehot = OneHotEncoder()
-
+# onehot = OneHotEncoder(sparse_output=False)
+#
 df = pd.read_csv('cleaned.csv')
-print(df.head())
-print(df.Locality)
+# print(df.head())
+#
+# df2 = onehot.fit_transform(df.Locality.values.reshape(-1, 1))
+# print(df2)
 
-dummies = pd.get_dummies(df.Locality)
-print(dummies.head())
+
+# transform
+onehot_transformer = OneHotEncoder(sparse_output=False,drop='first')
+transform1_features = ['Locality','Furnishing']
+transformer = ColumnTransformer([
+    ('transform1',onehot_transformer,transform1_features)
+],remainder='passthrough')
+
+print(len(df.Locality.unique()))
+print(len(df.Furnishing.unique()))
+df = transformer.fit_transform(df)
+print(df)
