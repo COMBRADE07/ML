f3a33999246c7f87bdee702ff6a09d06214475de
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-10-09 22:10:06 +0530

    d

--- Ml_revision/project/house_price_predictor.py
+++ Ml_revision/project/house_price_predictor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, LogisticRegression, Lasso, Ridge
 
@@ -76,7 +76,7 @@ def check_outliers(columns):
         sns.boxplot(data=df[column])
         plt.title(f'Box Plot for {column}')
     plt.show()
-check_outliers(columns)
+# check_outliers(columns)
 # fixing outlier
 
 def remove_outliers(df):
@@ -98,3 +98,7 @@ def remove_outliers(df):
 
 x = remove_outliers(df)
 print(x.head())
+
+one = OneHotEncoder()
+z = one.fit_transform([df.Furnishing])
+print(z)
